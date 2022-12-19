534b67eb6e8fd7d770753bca50f108bac171f719
gloria <enyonamlaryea154@gmail.com>
2022-12-19 18:04:02 +0000

    latest questions

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = " , "
+    for latest_question in latest_question_list:
+        output.append(latest_question.question_text)
+        print("question_text", latest_question.question_text) 
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
