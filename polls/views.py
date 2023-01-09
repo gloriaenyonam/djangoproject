a09f4cde6dba52832bc3bc5f579b9c4a209e5969
gloria <enyonamlaryea154@gmail.com>
2023-01-09 17:27:54 +0000

    dealing with vote fnx logic

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import Http404
-from .models import Question
+from .models import Question,Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list
         }
@@ -29,5 +32,23 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = Question.objects.get(pk=question_id)
+    #print("voters",request.POST['choice'])
+    try:
+        selected_choice = Choice.objects.get(pk=request.POST['choice'])
+        
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
 
